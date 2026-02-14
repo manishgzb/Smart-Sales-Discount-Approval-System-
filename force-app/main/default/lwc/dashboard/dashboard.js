3c4bbad34019ad17c14413e013b6e431d7004dc1
Manish Kumar <manishkumarmdvpic@gmail.com>
2026-02-14 11:46:49 +0530

    Add Dashboard Summary Cards

--- force-app/main/default/lwc/dashboard/dashboard.js
+++ force-app/main/default/lwc/dashboard/dashboard.js
@@ -1,4 +1,17 @@
+import { LightningElement,wire } from 'lwc';
+import getOppCounts from '@salesforce/apex/OpportunityController.getOppCounts';
+import getWonOppCounts from '@salesforce/apex/OpportunityController.getWonOppCounts';
+import getLostOppCounts from '@salesforce/apex/OpportunityController.getLostOppCounts';
+import getPendingDiscountRequestCount from '@salesforce/apex/DiscountController.getPendingDiscountRequestCount';
 
-import { LightningElement } from 'lwc';
+export default class Dashboard extends LightningElement {
+    @wire(getOppCounts)
+    oppCounts;
+    @wire(getWonOppCounts)
+    wonOppCounts;
+    @wire(getLostOppCounts)
+    lostOppCounts;
+    @wire(getPendingDiscountRequestCount)
+    pendingDiscountRequestCounts;
 
-export default class Dashboard extends LightningElement {}
+}
